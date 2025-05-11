4ecce108247d999e6d0c595d5a44a00a948e83fa
T12ys <155729923+T12ys@users.noreply.github.com>
2025-05-11 18:50:25 +0400

    Initial commit from Rider

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Write the number from 1 to 100: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 100)
+{
+    Console.WriteLine("Please enter a number between 1 and 100");
+}
+else if (number % 3 == 0 && number % 5 == 0)
+{
+    Console.WriteLine("FizzBuzz");
+}
+else if (number % 5 == 0)
+{
+    Console.WriteLine("Buzz");
+}
+else if (number % 3 == 0)
+{
+    Console.WriteLine("Fizz");
+}
